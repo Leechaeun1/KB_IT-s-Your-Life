38505c537da2116d68e3f5fc472fa3c9187a9440
채은 <88484000+Leechaeun1@users.noreply.github.com>
2025-03-14 17:40:11 +0900

    3/14

---
+++ kb_fullstack_실습/chapter02/02-14.js
@@ -0,0 +1,15 @@
+let obj = { ressult: 0 };
+
+obj.add = function (x, y) {
+  function inner() {
+    this.result = x + y;
+  }
+
+  // bind를 통해서 this 값을 지정한다
+  //add의 this를 inner의 this에 지정
+  inner = inner.bind(this);
+  inner();
+};
+obj.add(3, 4);
+
+console.log(obj);
---
+++ kb_fullstack_실습/chapter02/add_class.js
@@ -0,0 +1,9 @@
+class User {
+  constructor(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+  }
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
