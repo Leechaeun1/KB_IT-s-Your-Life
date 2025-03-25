514216e84249430194783a9c721307a9f09622e8
Leechaeun1 <lce001105@gmail.com>
2025-03-25 21:56:05 +0900

    changedProb

---
+++ kb_fullstack_실습/02_VUE/02_VUE/3_25_prob/prob/src/components/MenuList.vue
@@ -0,0 +1,15 @@
+<template>
+  <button
+    type="button"
+    @click="$emit('selectDrink', { name })"
+    class="btn btn-dark"
+  >
+    {{ name }} ({{ price }}원)
+  </button>
+</template>
+<script>
+export default {
+  name: 'MenuList',
+  props: ['name', 'price'],
+};
+</script>
