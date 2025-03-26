6136d6fb5cbbc2733f7163dfc7e65f4775260f88
Chae Eun <lce001105@gmail.com>
2025-03-26 17:32:32 +0900

    실습2ing...

--- kb_fullstack_실습/02_VUE/02_VUE/3_26_prob/improv-acting-lab/src/App.vue
+++ kb_fullstack_실습/02_VUE/02_VUE/3_26_prob/improv-acting-lab/src/App.vue
@@ -19,7 +19,36 @@ export default {
   name: 'App',
   components: { LineReadyButton },
   data() {
-    return { readyCount: 0 };
+    return {
+      readyCount: 0,
+      lines: [
+        {
+          line: '조금은 친절해도 되잖아. 다들 니들처럼 익숙한 게 아니니까.',
+          source: '청춘시대',
+        },
+        {
+          line: '너와 함께한 시간 모두 눈부셨다. 날이 좋아서 날이 좋지 않아서 날이 적당해서 모든 날이 좋았다.',
+          source: '도깨비',
+        },
+        {
+          line: '당신이 좋은 이유? 그저 그 사람이라서. 바로 너라서.',
+          source: '응답하라 1997',
+        },
+        {
+          line: '인생에서 시련 오는 거, 진짜와 가짜를 한 번씩 걸러내라고 하나님이 주신 기회가 아닌가 싶다.',
+          source: '별에서 온 그대',
+        },
+        {
+          line: '잊지 말자. 나는 어머니의 자부심이다. 모자라고 부족한 자식이 아니다.',
+          source: '미생',
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      lines: {},
+    };
   },
   methods: {
     readyHandler(e) {
--- kb_fullstack_실습/02_VUE/02_VUE/chapter07/test-vue-app/src/main.js
+++ kb_fullstack_실습/02_VUE/02_VUE/chapter07/test-vue-app/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import App from './App3.vue';
+import App from './App2.vue';
 
 // 전역 컴포넌트 등록 방법
 // 모든 것에 등록이 되기 때문에 파일이 커질 수 있다는 단점 => 비권장
